485cbf1caf703883d5403f8402a84be56c2e8919
ClasherV <vaibhavraikwar505@gmail.com>
2024-11-10 21:34:49 +0530

    Update again

---
+++ Concepts/Scripts/Scripts without Escape Character/1. Introduction/Assignments/Assignment2.java
@@ -0,0 +1,97 @@
+/*
+// Q1. Write a program to print your name. For example print “My name is [your name].”
+
+class Assignment1
+{
+    public static void main(String[] args)
+    {
+        System.out.print("My Name is: Vaibhav Raikwar");
+    }
+}
+
+// O/p: My Name is: Vaibhav Raikwar
+*/
+
+/* 
+// Q2. Write a Java program to print following messages each on new line:
+//     Java is fun!
+//     I am learning Java from Jenny’s Lectures!
+
+class Assignment1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Java is fun!");
+        System.out.print("I am learning Java from Jenny's Lectures!");
+    }
+}
+
+// O/p: Java is fun!
+//      I am learning Java from Jenny's Lectures!
+*/
+
+/* 
+// Q.3 Write a Java program that prints the numbers 1 to 5, each on a new line using System.out.println().
+
+class Assignment1
+{
+    public static void main(String[] args)
+    {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+        System.out.println(5);
+    }
+}
+
+// O/p: 1
+//      2
+//      3
+//      4
+//      5
+*/
+
+/* 
+// Q4 Write a Java program that uses more than one System.out.print() statements to print the line "I love programming!" in a single line, with spaces between the words.
+
+class Assignment1
+{
+    public static void main(String[] args)
+    {
+        System.out.print("I");
+        System.out.print(" love");
+        System.out.print(" programming!");
+    }
+}
+
+// O/p: I love programming!
+*/
+
+/* 
+// Q5 Write a program to print following patterns:  (using multiple System.out.println() statements)
+// *                                     *
+// * *                                 * *
+// * * *						     * * *
+// * *                                 * *
+// *                                     *
+
+
+class Assignment1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*               *");
+        System.out.println("* *           * *");
+        System.out.println("* * *       * * *");
+        System.out.println("* *           * *");
+        System.out.println("*               *");
+    }
+}
+
+// O/p: *               *
+//      * *           * *
+//      * * *       * * *
+//      * *           * *
+//      *               *
+*/
